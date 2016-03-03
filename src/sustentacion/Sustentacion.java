ca1125bbecc2fa6a98d2c721497674c5232d8b35
ANIBAL <ANIBAL@Anibal>
2016-03-03 11:23:54 -0500

    version 3.0 software sustentacion.

--- src/sustentacion/Sustentacion.java
+++ src/sustentacion/Sustentacion.java
@@ -10,11 +10,11 @@ public class Sustentacion {
      public static void main(String[] args) {
         PersonalData Datos[] = new PersonalData[100];
         Scanner lector = new Scanner (System.in);
-        int opc, contUs=0, band=0;
+        int opc, contUs=0, band=0, opcE;
         String name;
         
         do{
-            System.out.println("\nMenú principal.");
+            System.out.println("\n\nMenú principal.");
             System.out.println("1. Ingresar Usuario.");
             System.out.println("2. Actualizar Usuario.");
             System.out.println("3. Eliminar Usuario.");
@@ -25,23 +25,22 @@ public class Sustentacion {
             
             switch (opc){
                 case 1:
-                    if(contUs<99){
-                        
+                    if(contUs<99){                        
                     Datos[contUs] = new PersonalData();   
-                    System.out.println("Ingreso de usuarios.");
-                    System.out.print("Digite el nombre del usuario: ");
+                    System.out.println("\nIngreso de usuarios.");
+                    System.out.print("\nDigite el nombre del usuario: ");
                     Datos[contUs].setNombre(lector.next());
-                    System.out.print("Digite el apellido del usuario: ");
+                    System.out.print("\nDigite el apellido del usuario: ");
                     Datos[contUs].setApellido(lector.next());
-                    System.out.print("Digite la fecha de nacimiento del usuario: ");
+                    System.out.print("\nDigite la fecha de nacimiento del usuario: ");
                     Datos[contUs].setFecha(lector.next());
-                    System.out.print("Digite la cedula del usuario: ");
+                    System.out.print("\nDigite la cedula del usuario: ");
                     Datos[contUs].setCedula(lector.next());
-                    System.out.print("Digite la dirección del usuario: ");
+                    System.out.print("\nDigite la dirección del usuario: ");
                     Datos[contUs].setDireccion(lector.next());
-                    System.out.print("Digite el teléfono del usuario: ");
+                    System.out.print("\nDigite el teléfono del usuario: ");
                     Datos[contUs].setTelefono(lector.next());
-                    System.out.print("Digite el área de interes del usuario: química, física, tecnológia, cálculo o programación: ");
+                    System.out.print("\nDigite el área de interes del usuario: química, física, tecnológia, cálculo o programación: ");
                     Datos[contUs].setAreas(lector.next());
                     contUs++;
                     }
@@ -69,10 +68,62 @@ public class Sustentacion {
                     else 
                         band=0;
                     
-                    break;
+                    break;                            
                 case 3:
+                    System.out.println("\nEliminar usuario.");
+                    System.out.println("\nDigite el nombre a eliminar: ");
+                    name = lector.next();
+                    
+                    System.out.println("\n¿Seguro que desea eliminar el usuario?");
+                    System.out.println("\n  1. Para Confirmar.");
+                    System.out.println("\n  2. Para Cancelar.");
+                    opcE = lector.nextInt();
+                    
+                    if(opcE==1){
+                        
+                        for (int i=0; i<contUs; i++){
+                        if (name.equals(Datos[i].getNombre())){
+                            band=1;
+                                                                
+                    Datos[i] = null;
+                    
+                    System.out.println("\nUsuario eliminado.");
+                           
+                        }
+                                               
+                    }
+                    if (band==0)
+                        System.out.print("Usuario no encontrado");
+                    else 
+                        band=0;
+                                                
+                    }else                 
+                    
                     break;
                 case 4:
+                    System.out.println("\nEliminar usuario.");
+                    System.out.println("\nDigite el nombre a buscar: ");
+                    name = lector.next();
+                    
+                    for (int i=0; i<contUs; i++){
+                        if (name.equals(Datos[i].getNombre())){
+                            band=1;
+                            
+                    System.out.print("\nNombre de usuario: "+Datos[i].getNombre());
+                    System.out.print("\nApellido: "+Datos[i].getApellido());
+                    System.out.print("\nFecha de nacimiento: "+Datos[i].getFecha());
+                    System.out.print("\nCedula: "+Datos[i].getCedula());
+                    System.out.print("\nDirección: "+Datos[i].getDireccion());
+                    System.out.print("\nTelefono: "+Datos[i].getTelefono());
+                    System.out.print("\nArea de interés: "+Datos[i].getAreas());
+                                                                
+                        }
+                    }
+                    if (band==0)
+                        System.out.print("Usuario no encontrado");
+                    else 
+                        band=0;
+                    
                     break;
                 case 5:
                     System.out.println("Gracias por utilizar el programa.");
